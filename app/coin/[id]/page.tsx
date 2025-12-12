46d9658a366306d2dfb41cea9bfb1f584e4e7a36
lamak-shahiwala <shahiwalalamak@gmail.com>
2025-12-12 16:53:21 +0530

    appcoin page init

---
+++ app/coin/[id]/page.tsx
@@ -0,0 +1,13 @@
+import GeckoTerminal from "@/components/GeckoTerminal";
+import Navbar from "@/components/NavBar";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full ">
+      <Navbar />
+      <main className="grid grid-cols-2 h-[86vh] w-full justify-between items-center">
+        
+      </main>
+    </div>
+  );
+}
