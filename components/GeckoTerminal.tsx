861dae736da20fdc3ffa817ae0d4f0059ba2fd44
lamak-shahiwala <shahiwalalamak@gmail.com>
2025-12-12 16:46:55 +0530

    GeckoTerminal init

---
+++ components/GeckoTerminal.tsx
@@ -0,0 +1,13 @@
+export default function GeckoTerminal() {
+  return (
+    <iframe
+      id="geckoterminal-embed"
+      title="GeckoTerminal Embed"
+      src="https://www.geckoterminal.com/solana/pools/2zMMhcVQEXDtdE6vsFS7S7D5oUodfJHE8vd1gnBouauv?embed=1&info=0&swaps=0&light_chart=1&chart_type=price&resolution=5m&bg_color=f1f5f9"
+      frameBorder="0"
+      allow="clipboard-write"
+      allowFullScreen
+      className="w-full h-full"
+    />
+  );
+}
